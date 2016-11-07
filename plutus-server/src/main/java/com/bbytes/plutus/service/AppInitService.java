22868e35133290434e13da790bf306a89be0e470
tanmally <tm@beyondbytes.co.in>
2016-11-07 15:28:43 +0530

    Added app init and it will add the default users

---
+++ plutus-server/src/main/java/com/bbytes/plutus/service/AppInitService.java
@@ -0,0 +1,27 @@
+package com.bbytes.plutus.service;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bbytes.plutus.model.PlutusUser;
+
+@Service
+public class AppInitService {
+
+	@Autowired
+	private UserService userService;
+
+	@PostConstruct
+	protected void init() {
+		PlutusUser plutusUser1 = new PlutusUser("test", "test@test.com", "test");
+		plutusUser1.setPassword("test");
+
+		PlutusUser plutusUser2 = new PlutusUser("admin", "admin@admin.com", "admin");
+		plutusUser2.setPassword("admin");
+
+		userService.save(plutusUser1);
+		userService.save(plutusUser2);
+	}
+}
