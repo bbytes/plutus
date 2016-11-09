d28cc1f3bc8380fea82599b0d9ea20f9e4508375
Jayashree <jayashree@beyondbytes.co.in>
2016-11-09 22:37:34 +0530

    get stats details of particular customer and added timeperiod tab in subscription

--- plutus-ui/source/app/services/subscriptionService.js
+++ plutus-ui/source/app/services/subscriptionService.js
@@ -56,5 +56,42 @@ angular.module('plutusApp').service('subscriptionService', function ($rootScope,
 
         return deferred.promise;
     };
+    
+    this.getAllTimePeriods = function () {
+
+        var deferred = $q.defer();
+
+        $http({
+            method: 'GET',
+            url: $rootScope.baseUrl + 'api/v1/dropdown/timePeriod',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).success(function (response, status, headers, config) {
+            deferred.resolve(response);
+        }).error(function () {
+            deferred.reject({'success': false, 'msg': 'Oops! Something went wrong. Please try again later.'});
+        });
+
+        return deferred.promise;
+    };
    
+     this.getCustomerDetails = function (key,timeperiod) {
+
+        var deferred = $q.defer();
+
+        $http({
+            method: 'GET',
+            url: $rootScope.baseUrl + 'api/v1/product/stats/' + timeperiod  + '/' + key,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).success(function (response, status, headers, config) {
+            deferred.resolve(response);
+        }).error(function () {
+            deferred.reject({'success': false, 'msg': 'Oops! Something went wrong. Please try again later.'});
+        });
+
+        return deferred.promise;
+    };
 });
